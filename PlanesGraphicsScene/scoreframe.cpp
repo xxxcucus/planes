8283c5779c1bf94fc2bb7ce438867f4def408689
Cristian Cucu <Cristian Cucu>
2017-04-02 19:55:47 +0200

    Added title to general score frame.

--- PlanesGraphicsScene/scoreframe.cpp
+++ PlanesGraphicsScene/scoreframe.cpp
@@ -4,17 +4,21 @@
 
 ScoreFrame::ScoreFrame(QWidget* parent): QFrame(parent)
 {
+    QString titleText = QString("<b> General Score</b>");
+    QLabel* titleLabel = new QLabel();
+    titleLabel->setText(titleText);
     QLabel* computerScoreTextLabel = new QLabel("Computer Wins: ");
     QLabel* playerScoreTextLabel = new QLabel("Player wins: ");
     m_ComputerScoreLabel = new QLabel("0");
     m_PlayerScoreLabel = new QLabel("0");
     m_StartGameButton = new QPushButton("Start new game");
     QGridLayout* gridLayout1 = new QGridLayout();
-    gridLayout1->addWidget(computerScoreTextLabel, 0, 0);
-    gridLayout1->addWidget(playerScoreTextLabel, 1, 0);
-    gridLayout1->addWidget(m_ComputerScoreLabel, 0, 1);
-    gridLayout1->addWidget(m_PlayerScoreLabel, 1, 1);
-    gridLayout1->addWidget(m_StartGameButton, 2, 0);
+    gridLayout1->addWidget(titleLabel);
+    gridLayout1->addWidget(computerScoreTextLabel, 1, 0);
+    gridLayout1->addWidget(playerScoreTextLabel, 2, 0);
+    gridLayout1->addWidget(m_ComputerScoreLabel, 1, 1);
+    gridLayout1->addWidget(m_PlayerScoreLabel, 2, 1);
+    gridLayout1->addWidget(m_StartGameButton, 3, 0);
     setLayout(gridLayout1);
     setFrameStyle(QFrame::Panel | QFrame::Raised);
 }
