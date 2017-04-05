8a6a0ce98cebd43204d8700ffa08b0d231ba4521
Cristian Cucu <Cristian Cucu>
2017-04-05 20:23:52 +0200

    Changed shapes of squares outside the playing grid.

--- PlanesGraphicsScene/gridsquare.cpp
+++ PlanesGraphicsScene/gridsquare.cpp
@@ -12,21 +12,27 @@ void GridSquare::paint(QPainter* painter, const QStyleOptionGraphicsItem* option
     Q_UNUSED(option)
     Q_UNUSED(widget)
 
+    QPainterPath fillPath;
+    fillPath.addRoundedRect(boundingRect().adjusted(m_Width/10, m_Width/10, -m_Width/10, -m_Width/10), 5, 5);
+    painter->setRenderHint(QPainter::Antialiasing);
+
     if (!m_Selected) {
         switch(m_Type) {
             case Type::Empty:
-                painter->fillRect(boundingRect(), Qt::yellow);
+                painter->fillPath(fillPath, Qt::yellow);
+                painter->strokePath(fillPath, QPen(Qt::black));
                 break;
             case Type::PlaneHead:
-                painter->fillRect(boundingRect(), Qt::green);
+                painter->fillPath(fillPath, Qt::green);
+                painter->strokePath(fillPath, QPen(Qt::black));
                 break;
             case Type::Plane:
-                painter->fillRect(boundingRect(), m_Color);
+                painter->fillPath(fillPath, m_Color);
+                painter->strokePath(fillPath, QPen(Qt::black));
                 break;
         }
     } else {
-        painter->fillRect(boundingRect(), Qt::green);
+        painter->fillPath(fillPath, Qt::blue);
+        painter->strokePath(fillPath, QPen(Qt::black));
     }
-    painter->setPen(Qt::black);
-    painter->drawRect(boundingRect());
 }
