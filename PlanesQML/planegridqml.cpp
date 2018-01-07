a4915cb6e7196e1f6c221c90ebd26b098e4d8bbe
Cristian Cucu <xxxcucus@yahoo.com>
2018-01-07 16:26:08 +0100

    Refactoring m_PlaneGrid is now a pointer.

--- PlanesQML/planegridqml.cpp
+++ PlanesQML/planegridqml.cpp
@@ -4,9 +4,9 @@
 QColor PlaneGridQML::getPlanePointColor(int idx) const
 {
    ///@todo: mark the head of the planes with green
-   int colorStep = (m_MaxPlaneBodyColor - m_MinPlaneBodyColor) / m_PlaneGrid.getPlaneNo();
-   int annotation = m_PlaneGrid.getPlanePointAnnotation(idx);
-   std::vector<int> planesIdx = m_PlaneGrid.decodeAnnotation(annotation);
+   int colorStep = (m_MaxPlaneBodyColor - m_MinPlaneBodyColor) / m_PlaneGrid->getPlaneNo();
+   int annotation = m_PlaneGrid->getPlanePointAnnotation(idx);
+   std::vector<int> planesIdx = m_PlaneGrid->decodeAnnotation(annotation);
    if (planesIdx.size() > 1)    //point belongs to more planes mark it with red
        return QColor(255, 0, 0);
    if (planesIdx.size() == 1) {
--- PlanesQML/planegridqml.h
+++ PlanesQML/planegridqml.h
@@ -13,70 +13,70 @@ class PlaneGridQML : public QObject
     Q_OBJECT
 
 public:
-    PlaneGridQML(int rows, int cols, int planesNo, bool isComputer): m_PlaneGrid(rows, cols, planesNo, isComputer) {
-        connect(&m_PlaneGrid, SIGNAL(planesPointsChanged()), this, SIGNAL(planesPointsChanged()));
+    PlaneGridQML(int rows, int cols, int planesNo, bool isComputer): m_PlaneGrid(new PlaneGrid(rows, cols, planesNo, isComputer)) {
+        connect(m_PlaneGrid, SIGNAL(planesPointsChanged()), this, SIGNAL(planesPointsChanged()));
     }
 
     Q_INVOKABLE int getPlanesPointsCount() const {
-        return m_PlaneGrid.getPlanesPointsCount();
+        return m_PlaneGrid->getPlanesPointsCount();
     }
 
     Q_INVOKABLE QPoint getPlanePoint(int idx) const {
-        return m_PlaneGrid.getPlanePoint(idx);
+        return m_PlaneGrid->getPlanePoint(idx);
     }
 
     Q_INVOKABLE QColor getPlanePointColor(int idx) const;
 
     Q_INVOKABLE int getRows() const {
-        return m_PlaneGrid.getRowNo();
+        return m_PlaneGrid->getRowNo();
     }
 
     Q_INVOKABLE int getCols() const {
-        return m_PlaneGrid.getColNo();
+        return m_PlaneGrid->getColNo();
     }
 
     Q_INVOKABLE int getPlanesNo() const {
-        return m_PlaneGrid.getPlaneNo();
+        return m_PlaneGrid->getPlaneNo();
     }
 
     Q_INVOKABLE bool getIsComputer() const {
-        return m_PlaneGrid.isComputer();
+        return m_PlaneGrid->isComputer();
     }
 
     Q_INVOKABLE void toggleSelectedPlane() {
-        m_SelectedPlane = (m_SelectedPlane + 1) % m_PlaneGrid.getPlaneNo();
+        m_SelectedPlane = (m_SelectedPlane + 1) % m_PlaneGrid->getPlaneNo();
         emit planesPointsChanged();
     }
 
     Q_INVOKABLE void rotateSelectedPlane() {
-        m_PlaneGrid.rotatePlane(m_SelectedPlane);
+        m_PlaneGrid->rotatePlane(m_SelectedPlane);
     }
 
     Q_INVOKABLE void moveUpSelectedPlane() {
-        m_PlaneGrid.movePlaneUpwards(m_SelectedPlane);
+        m_PlaneGrid->movePlaneUpwards(m_SelectedPlane);
     }
 
     Q_INVOKABLE void moveDownSelectedPlane() {
-        m_PlaneGrid.movePlaneDownwards(m_SelectedPlane);
+        m_PlaneGrid->movePlaneDownwards(m_SelectedPlane);
     }
 
     Q_INVOKABLE void moveLeftSelectedPlane() {
-        m_PlaneGrid.movePlaneLeft(m_SelectedPlane);
+        m_PlaneGrid->movePlaneLeft(m_SelectedPlane);
     }
 
     Q_INVOKABLE void moveRightSelectedPlane() {
-        m_PlaneGrid.movePlaneRight(m_SelectedPlane);
+        m_PlaneGrid->movePlaneRight(m_SelectedPlane);
     }
 
     inline void initGrid() {
-        m_PlaneGrid.initGrid();
+        m_PlaneGrid->initGrid();
     }
 
 signals:
     void planesPointsChanged();
 
 private:
-    PlaneGrid m_PlaneGrid;
+    PlaneGrid* m_PlaneGrid;
 
     ///grey colors to draw the planes
     int m_MinPlaneBodyColor = 0;
