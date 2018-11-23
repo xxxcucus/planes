40cc54d679ad4198174fce6dd7849ed6ec5da8c7
Cristian Cucu <cristian.cucu@hm-ag.de>
2018-11-23 08:56:54 +0100

    Extracted HeadData structure in separate files.

--- common/computerlogic.h
+++ common/computerlogic.h
@@ -7,6 +7,7 @@
 #include "planeiterators.h"
 #include "coordinate2d.h"
 #include "planeorientationdata.h"
+#include "headdata.h"
 
 //The computer is trying to guess where the player's planes are
 //For every position on the table and each of the 4 plane orientations
@@ -20,27 +21,6 @@
 //Dead - the guess is the plane's head
 //describes the data that is available about a given plane position
 //This structure keeps the information about the position of the head of the planes
-
-struct HeadData
-{
-    //size of the grid
-    int m_row, m_col;
-    //position of the head
-    int m_headRow, m_headCol;
-    //the correct plane orientation if decided
-    int m_correctOrient;
-
-
-    //statistics about the 4 positions with this head
-    PlaneOrientationData m_options[4];
-
-    HeadData(int row, int col, int headRow, int headCol);
-    //update the current data with a guess
-    //return true if a plane is confirmed
-    bool update(const GuessPoint& gp);
-};
-
-
 
 //implements the logic of computer's game
 class ComputerLogic
