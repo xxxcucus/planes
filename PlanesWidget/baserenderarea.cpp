918092ae6187e57b259fea1e53151440276a7583
Cristian Cucu <cristian.cucu@hm-ag.de>
2018-11-28 10:17:00 +0100

    Coding style.

--- PlanesWidget/baserenderarea.cpp
+++ PlanesWidget/baserenderarea.cpp
@@ -5,16 +5,13 @@
 
 
 //constructs the class object
-
 BaseRenderArea::BaseRenderArea(int row, int col, QWidget *parent):
     QWidget(parent),
     m_rowNo(row), m_colNo(col),
     m_spacing(20),
     m_offsetRow(20),
     m_offsetCol(20)
-
 {
-
     //sets the size policy as expanding ; we want the widget to nicely resize when we resize the widget
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     setWindowTitle("");
@@ -25,22 +22,19 @@ BaseRenderArea::BaseRenderArea(int row, int col, QWidget *parent):
 
 QSize BaseRenderArea::minimumSizeHint() const
 {
-    return calculateSizeHint(20,20,20);
+    return calculateSizeHint(20, 20, 20);
 }
 
 //calculates the size hint for the widget
-
 QSize BaseRenderArea::sizeHint() const
 {
     return calculateSizeHint(m_spacing, m_offsetRow, m_offsetCol);
-
 }
-
 
 //calculates the proper sizeHint
 QSize BaseRenderArea::calculateSizeHint(int spacing, int offsetRow, int offsetCol) const
 {
-    return QSize((m_rowNo+2)*spacing+offsetRow, (m_colNo+1)*spacing+offsetCol);
+    return QSize((m_rowNo + 2) * spacing + offsetRow, (m_colNo + 1) * spacing + offsetCol);
 }
 
 //draws the grid, the numbers on the side and the title
@@ -66,17 +60,17 @@ void BaseRenderArea::paintEvent(QPaintEvent* /*event*/)
 void BaseRenderArea::calculateDimensions()
 {
     //numbers of rows and columns left as margin
-    int noRowMargin = m_rowNo/8;
-    int noColMargin = m_colNo/8;
+    int noRowMargin = m_rowNo / 8;
+    int noColMargin = m_colNo / 8;
     //total number of rows and columns including margin
-    int noVirtualRows = m_rowNo+2+2*noRowMargin;
-    int noVirtualCols = m_colNo+1+2*noColMargin;
+    int noVirtualRows = m_rowNo + 2 + 2 * noRowMargin;
+    int noVirtualCols = m_colNo + 1 + 2 * noColMargin;
 
     //the spacing between the lines
-    m_spacing = std::min(this->geometry().height()/noVirtualRows, this->geometry().width()/noVirtualCols);
+    m_spacing = std::min(this->geometry().height()/noVirtualRows, this->geometry().width() / noVirtualCols);
     //the offset of the grid with respect to the widget
-    m_offsetRow = (this->geometry().width()- m_spacing*(m_colNo+1))/2;
-    m_offsetCol = (this->geometry().height() - m_spacing*(m_rowNo+2))/2;
+    m_offsetRow = (this->geometry().width()- m_spacing * (m_colNo + 1)) / 2;
+    m_offsetCol = (this->geometry().height() - m_spacing * (m_rowNo + 2)) / 2;
 
 }
 
@@ -87,12 +81,12 @@ void BaseRenderArea::drawGrid(QPainter *painter) const
 {
 
     //draw horizontal lines
-    for(int i=0;i<=m_colNo;i++)
-        painter->drawLine(m_offsetRow+m_spacing,(i+2)*m_spacing+m_offsetCol,(m_rowNo+1)*m_spacing+m_offsetRow,(i+2)*m_spacing+m_offsetCol);
+    for(int i = 0; i <= m_colNo; i++)
+        painter->drawLine(m_offsetRow + m_spacing,(i+2) * m_spacing + m_offsetCol,(m_rowNo + 1) * m_spacing + m_offsetRow,(i + 2) * m_spacing + m_offsetCol);
 
     //draw vertical lines
-    for(int i=0;i<=m_rowNo;i++)
-        painter->drawLine((i+1)*m_spacing+m_offsetRow,m_offsetCol+2*m_spacing,(i+1)*m_spacing+m_offsetRow,(m_colNo+2)*m_spacing+m_offsetCol);
+    for(int i = 0; i <= m_rowNo; i++)
+        painter->drawLine((i + 1) * m_spacing + m_offsetRow, m_offsetCol + 2 * m_spacing,(i + 1) * m_spacing + m_offsetRow, (m_colNo + 2) * m_spacing + m_offsetCol);
 }
 
 //draw texts and title
@@ -100,26 +94,26 @@ void BaseRenderArea::drawGrid(QPainter *painter) const
 void BaseRenderArea::drawTexts(QPainter *painter) const
 {
     //draw vertical texts
-    for(int i=0;i<m_colNo;i++)
-        painter->drawText(m_offsetRow,(i+2)*m_spacing+m_offsetCol+m_spacing*2/3,QString::number(i));
+    for(int i = 0; i < m_colNo; i++)
+        painter->drawText(m_offsetRow,(i + 2) * m_spacing + m_offsetCol + m_spacing * 2 / 3,QString::number(i));
 
     //draw horizontal texts
-    for(int i=0;i<m_rowNo;i++)
-        painter->drawText((i+1)*m_spacing+m_offsetRow,m_offsetCol+3*m_spacing/2,QString::number(i));
+    for(int i = 0;i < m_rowNo; i++)
+        painter->drawText((i + 1) * m_spacing + m_offsetRow, m_offsetCol + 3 * m_spacing / 2,QString::number(i));
 
     //displays the window title
     QFontMetrics fm = this->fontMetrics();
     int titleLength = fm.width(windowTitle());
 
 
-    painter->drawText(m_rowNo/2*m_spacing+m_spacing+ m_offsetRow-titleLength/2, m_offsetCol, windowTitle());
+    painter->drawText(m_rowNo / 2 * m_spacing + m_spacing + m_offsetRow - titleLength / 2, m_offsetCol, windowTitle());
 }
 
 
 //checks whether a given position is in grid
 bool BaseRenderArea::posInGrid(int row, int col) const
 {
-    if(row<0 || row>=m_rowNo || col<0 || col>=m_colNo)
+    if(row < 0 || row >= m_rowNo || col < 0 || col >= m_colNo)
         return false;
 
     return true;
@@ -131,10 +125,10 @@ bool BaseRenderArea::posInGrid(int row, int col) const
 
 void BaseRenderArea::drawMiss(int row, int col, QPainter *painter) const
 {
-    if(!posInGrid(row,col))
+    if (!posInGrid(row,col))
         return;
 
-    painter->drawEllipse((row+1)*m_spacing+m_spacing/3+m_offsetRow, (col+2)*m_spacing+m_spacing/3+m_offsetCol, m_spacing/3, m_spacing/3);
+    painter->drawEllipse((row + 1) * m_spacing + m_spacing / 3 + m_offsetRow, (col + 2) * m_spacing + m_spacing / 3 + m_offsetCol, m_spacing / 3, m_spacing / 3);
 }
 
 //draw a hit
@@ -142,12 +136,12 @@ void BaseRenderArea::drawMiss(int row, int col, QPainter *painter) const
 
 void BaseRenderArea::drawHit(int row, int col, QPainter *painter) const
 {
-    if(!posInGrid(row,col))
+    if (!posInGrid(row,col))
         return;
 
-    painter->drawLine((row+1)*m_spacing+m_offsetRow, (col+3)*m_spacing+m_offsetCol, (row+1)*m_spacing+m_offsetRow+m_spacing/2, (col+2)*m_spacing+m_offsetCol);
-    painter->drawLine((row+1)*m_spacing+m_offsetRow+m_spacing/2, (col+2)*m_spacing+m_offsetCol, (row+2)*m_spacing+m_offsetRow, (col+3)*m_spacing+m_offsetCol);
-    painter->drawLine((row+2)*m_spacing+m_offsetRow, (col+3)*m_spacing+m_offsetCol, (row+1)*m_spacing+m_offsetRow, (col+3)*m_spacing+m_offsetCol);
+    painter->drawLine((row + 1) * m_spacing + m_offsetRow, (col + 3) * m_spacing + m_offsetCol, (row + 1) * m_spacing + m_offsetRow + m_spacing / 2, (col + 2) * m_spacing + m_offsetCol);
+    painter->drawLine((row + 1) * m_spacing + m_offsetRow + m_spacing / 2, (col + 2) * m_spacing + m_offsetCol, (row + 2) * m_spacing + m_offsetRow, (col + 3) * m_spacing + m_offsetCol);
+    painter->drawLine((row + 2) * m_spacing + m_offsetRow, (col + 3) * m_spacing + m_offsetCol, (row + 1 ) * m_spacing + m_offsetRow, (col + 3) * m_spacing + m_offsetCol);
 }
 
 //draw a dead
@@ -155,22 +149,21 @@ void BaseRenderArea::drawHit(int row, int col, QPainter *painter) const
 
 void BaseRenderArea::drawDead(int row, int col, QPainter *painter) const
 {
-    if(!posInGrid(row,col))
+    if (!posInGrid(row,col))
         return;
 
-    painter->drawLine((row+1)*m_spacing+m_offsetRow, (col+2)*m_spacing+m_offsetCol, (row+2)*m_spacing+m_offsetRow, (col+3)*m_spacing+m_offsetCol);
-    painter->drawLine((row+2)*m_spacing+m_offsetRow, (col+2)*m_spacing+m_offsetCol, (row+1)*m_spacing+m_offsetRow, (col+3)*m_spacing+m_offsetCol);
+    painter->drawLine((row + 1) * m_spacing + m_offsetRow, (col + 2) * m_spacing + m_offsetCol, (row + 2) * m_spacing + m_offsetRow, (col + 3) * m_spacing + m_offsetCol);
+    painter->drawLine((row + 2) * m_spacing + m_offsetRow, (col + 2) * m_spacing + m_offsetCol, (row + 1) * m_spacing + m_offsetRow, (col + 3) * m_spacing + m_offsetCol);
 
 }
 
 //draws a number in the grid
 void BaseRenderArea::drawNumber(int row, int col, int val, QPainter *painter) const
 {
-    if(!posInGrid(row,col))
+    if (!posInGrid(row,col))
         return;
 
-
-    painter->drawText(m_offsetRow+(row+1)*m_spacing, m_offsetCol+(col+2)*m_spacing,m_spacing,m_spacing, Qt::AlignHCenter, QString::number(val));
+    painter->drawText(m_offsetRow + (row + 1) * m_spacing, m_offsetCol + (col + 2) * m_spacing, m_spacing, m_spacing, Qt::AlignHCenter, QString::number(val));
 }
 
 //draws a list of guesses in the grid
@@ -205,9 +198,9 @@ void BaseRenderArea::drawGuesses(const std::vector<GuessPoint>& list, QPainter *
 //returns false if coordinates are not good and true otherwise
 bool BaseRenderArea::fillGridRect(int row, int col, QString color, QPainter *painter) const
 {
-    if(!posInGrid(row,col))
+    if (!posInGrid(row,col))
         return false;
 
-    painter->fillRect((row+1)*m_spacing+m_offsetRow,(col+2)*m_spacing+m_offsetCol,m_spacing,m_spacing,QColor(color));
+    painter->fillRect((row + 1) * m_spacing + m_offsetRow, (col + 2) * m_spacing + m_offsetCol, m_spacing, m_spacing, QColor(color));
     return true;
 }
