db60bbd46f34ec4793a8a9638c9969c3ca3160b2
Cristian Cucu <cristian.cucu@hm-ag.de>
2019-01-15 13:40:54 +0100

    Rewrite PlaneRound class to eliminate signal and slots.

---
+++ common/planeroundjavafx.cpp
@@ -0,0 +1,161 @@
+#include "planeroundjavafx.h"
+
+PlaneRoundJavaFx::PlaneRoundJavaFx(PlaneGrid* playerGrid, PlaneGrid* computerGrid, ComputerLogic* logic, bool isComputerFirst): 
+	m_isComputerFirst(isComputerFirst),
+	m_PlayerGrid(playerGrid),
+	m_ComputerGrid(computerGrid),
+	m_computerLogic(logic)
+{
+	reset();
+} 
+
+
+//resets the PlaneRound object
+void PlaneRoundJavaFx::reset()
+{
+	m_PlayerGrid->resetGrid();
+	m_ComputerGrid->resetGrid();
+
+	m_playerGuessList.clear();
+	m_computerGuessList.clear();
+
+	m_gameStats.reset();
+	m_computerLogic->reset();
+}
+
+void PlaneRoundJavaFx::initRound()
+{
+	m_PlayerGrid->initGrid();
+	m_ComputerGrid->initGrid();
+	m_State = GameStages::BoardEditing;
+	m_isComputerFirst = !m_isComputerFirst;
+	reset();
+}
+
+//TODO: do we need to evaluate the player guess here ?
+void PlaneRoundJavaFx::playerGuess(const GuessPoint& gp, PlayerGuessReaction& pgr)
+{
+	if (m_State != GameStages::Game)
+		return;
+
+	if (m_isComputerFirst) {
+		GuessPoint gpc = guessComputerMove();
+		updateGameStats(gpc, true);
+		pgr.m_ComputerMoveGenerated = true;
+		pgr.m_ComputerGuess = gpc;
+
+		//update the game statistics
+		updateGameStats(gp, false);
+		//add the player's guess to the list of guesses
+		//assume that the guess is different from the other guesses
+		m_playerGuessList.push_back(gp);
+	} else {
+		//update the game statistics
+		updateGameStats(gp, false);
+		//add the player's guess to the list of guesses
+		//assume that the guess is different from the other guesses
+		m_playerGuessList.push_back(gp);
+
+		GuessPoint gpc = guessComputerMove();
+		updateGameStats(gpc, true);
+		pgr.m_ComputerMoveGenerated = true;
+		pgr.m_ComputerGuess = gpc;
+	}
+
+	bool isPlayerWinner = false;
+	if (roundEnds(isPlayerWinner)) {
+		m_gameStats.updateWins(!isPlayerWinner);
+		pgr.m_RoundEnds = true;
+		m_State = GameStages::GameNotStarted;
+	} else {
+		pgr.m_RoundEnds = false;
+	}
+
+	pgr.m_GameStats = m_gameStats;
+}
+
+void PlaneRoundJavaFx::rotatePlane(int idx) {
+	if (m_State != GameStages::BoardEditing)
+		return;
+	m_PlayerGrid->rotatePlane(idx);
+}
+
+void PlaneRoundJavaFx::movePlaneLeft(int idx) {
+	if (m_State != GameStages::BoardEditing)
+		return;
+	m_PlayerGrid->movePlaneLeft(idx);
+}
+
+void PlaneRoundJavaFx::movePlaneRight(int idx) {
+	m_PlayerGrid->movePlaneRight(idx);
+}
+
+void PlaneRoundJavaFx::movePlaneUpwards(int idx) {
+	if (m_State != GameStages::BoardEditing)
+		return;
+	m_PlayerGrid->movePlaneUpwards(idx);
+}
+
+void PlaneRoundJavaFx::movePlaneDownwards(int idx) {
+	if (m_State != GameStages::BoardEditing)
+		return;
+	m_PlayerGrid->movePlaneDownwards(idx);
+}
+
+GuessPoint PlaneRoundJavaFx::guessComputerMove()
+{
+	PlanesCommonTools::Coordinate2D qp;
+	//use the computer strategy to get a move
+	m_computerLogic->makeChoice(qp);
+
+	//use the player grid to see the result of the grid
+	GuessPoint::Type tp = m_PlayerGrid->getGuessResult(qp);
+	GuessPoint gp(qp.x(), qp.y(), tp);
+
+	//add the data to the computer strategy
+	m_computerLogic->addData(gp);
+
+	//update the computer guess list
+	m_computerGuessList.push_back(gp);
+
+	return gp;
+}
+
+
+bool PlaneRoundJavaFx::roundEnds(bool& isPlayerWinner)
+{
+	//at equal scores computer wins
+	isPlayerWinner = false;
+
+	bool playerFinished = enoughGuesses(m_PlayerGrid, m_computerGuessList);
+	bool computerFinished = enoughGuesses(m_ComputerGrid, m_playerGuessList);
+
+	if (!computerFinished && playerFinished)
+		isPlayerWinner = true;
+
+	return (playerFinished || computerFinished);
+}
+
+//decides whether all the planes have been guessed
+bool PlaneRoundJavaFx::enoughGuesses(PlaneGrid* pg, const std::vector<GuessPoint>& guessList) const
+{
+	int count = 0;
+
+	for (unsigned int i = 0; i < guessList.size(); i++) {
+		GuessPoint gp = guessList.at(i);
+		if (gp.m_type == GuessPoint::Dead)
+			count++;
+	}
+
+	return (count >= pg->getPlaneNo());
+}
+
+//based on a guesspoint updates the game stats
+void PlaneRoundJavaFx::updateGameStats(const GuessPoint& gp, bool isComputer)
+{
+	m_gameStats.updateStats(gp, isComputer);
+}
+
+void PlaneRoundJavaFx::doneEditing() {
+	m_State = GameStages::Game;
+}
---
+++ common/planeroundjavafx.h
@@ -0,0 +1,75 @@
+#ifndef __PLANE_ROUND_JAVAFX_
+#define __PLANE_ROUND_JAVAFX_
+
+#include "planegrid.h"
+#include "computerlogic.h"
+#include "gamestatistics.h"
+#include "guesspoint.h"
+
+
+struct PlayerGuessReaction {
+	//victory for computer or player
+	bool m_RoundEnds = false;
+	//who won
+	bool m_isComputerWinner = false;
+	//if no victory then a computer move is generated
+	bool m_ComputerMoveGenerated = false;
+	//which computer move was generated 
+	GuessPoint m_ComputerGuess;
+	GameStatistics m_GameStats;
+};
+
+class PlaneRoundJavaFx {
+public:
+	enum class GameStages { GameNotStarted, BoardEditing, Game };
+
+	//constructs the round object
+	PlaneRoundJavaFx(PlaneGrid* playerGrid, PlaneGrid* computerGrid, ComputerLogic* logic, bool isComputerFirst);
+
+	//inits a new round
+	void initRound();
+
+	void playerGuess(const GuessPoint& gp, PlayerGuessReaction& pgr);
+
+	void rotatePlane(int idx);
+	void movePlaneLeft(int idx);
+	void movePlaneRight(int idx);
+	void movePlaneUpwards(int idx);
+	void movePlaneDownwards(int idx);
+
+	void doneEditing();
+
+private:
+	//update game statistics
+	void updateGameStats(const GuessPoint& gp, bool isComputer);
+	//tests whether all of the planes have been guessed
+	bool enoughGuesses(PlaneGrid* pg, const std::vector<GuessPoint>& guessList) const;
+	//based on the available information makes the next move for the computer
+	GuessPoint guessComputerMove();
+	//resets the round
+	void reset();
+	//is there a winner
+	bool roundEnds(bool& isPlayerWinner);
+
+private:
+	//whether the computer or the player moves first
+	bool m_isComputerFirst;
+	//the  game statistics
+	GameStatistics m_gameStats;
+
+	//the player and computer's grid
+	PlaneGrid* m_PlayerGrid;
+	PlaneGrid* m_ComputerGrid;
+
+	//the list of guesses for computer and player
+	std::vector<GuessPoint> m_computerGuessList;
+	std::vector<GuessPoint> m_playerGuessList;
+
+	//the computer's strategy
+	ComputerLogic* m_computerLogic;
+
+	GameStages m_State = GameStages::GameNotStarted;
+};
+
+
+#endif
