d3cd80dc6c0eb364836dc7bb5ff95490ac345308
Cristian Cucu <cristian.cucu@hm-ag.de>
2019-01-30 16:02:52 +0100

    Small fixes.

--- java/PlanesJavaFx/src/com/planes/javafx/PlaneRoundJavaFx.java
+++ java/PlanesJavaFx/src/com/planes/javafx/PlaneRoundJavaFx.java
@@ -3,9 +3,7 @@ package com.planes.javafx;
 //JNI class interfacing to the c++ game engine
 public class PlaneRoundJavaFx {
 	static {
-	      System.loadLibrary("libCommon"); // Load native library hello.dll (Windows) or libhello.so (Unixes)
-	                                   //  at runtime
-	                                   // This library contains a native method called sayHello()
+	      System.loadLibrary("libCommon"); // Load native library 
 	   }
 	 
 	// Declare an instance native method sayHello() which receives no parameter and returns void
