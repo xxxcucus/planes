00fcebff28a31a9b219e86610eac7ee7f0cbdd0b
Cristian Cucu <cristian.cucu@hm-ag.de>
2019-02-04 14:38:50 +0100

    Added activity and layout.

---
+++ java/PlanesAndroid/app/src/main/java/com/planes/android/PlanesAndroid.java
@@ -0,0 +1,12 @@
+package com.planes.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlanesAndroid extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
