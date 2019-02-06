e735c000b55f91c718010569aa4350df00dbc51b
Cristian Cucu <cristian.cucu@hm-ag.de>
2019-02-06 08:21:02 +0100

    Fixes

--- java/PlanesAndroid/app/src/main/java/com/planes/android/PlanesAndroidActivity.java
+++ java/PlanesAndroid/app/src/main/java/com/planes/android/PlanesAndroidActivity.java
@@ -2,16 +2,15 @@ package com.planes.android;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 public class PlanesAndroidActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        PlanesVerticalLayout parentLayout = new PlanesVerticalLayout(this);
+        setContentView(R.layout.activity_main);
 
-
-        setContentView(parentLayout);
+        //PlanesVerticalLayout parentLayout = new PlanesVerticalLayout(this);
+        //setContentView(parentLayout);
     }
 }
