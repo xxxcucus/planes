05019da7af1f41249f76c0a03c5ce46b52f502c2
Cristian Cucu <xxxcucus@yahoo.com>
2019-03-07 21:14:32 +0100

    Added popup show function

--- java/PlanesAndroid/app/src/main/java/com/planes/android/PlanesAndroidActivity.java
+++ java/PlanesAndroid/app/src/main/java/com/planes/android/PlanesAndroidActivity.java
@@ -3,8 +3,14 @@ package com.planes.android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.planes.javafx.PlaneRoundJavaFx;
 
@@ -77,6 +83,32 @@ public class PlanesAndroidActivity extends AppCompatActivity {
                 m_GameControls.setGameStage();
                 break;
         }
+    }
+
+    public void onButtonShowHelpWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.help_popup, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
     private PlaneRoundJavaFx m_PlaneRound;
