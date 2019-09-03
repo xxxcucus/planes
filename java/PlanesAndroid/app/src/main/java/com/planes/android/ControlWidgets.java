68e678aa4fd64ceefbed4018da89ae61a99c6865
Cristian Cucu <xxxcucus@yahoo.com>
2019-09-03 18:05:54 +0200

    Added empty definition for wrapper of controls

---
+++ java/PlanesAndroid/app/src/main/java/com/planes/android/ControlWidgets.java
@@ -0,0 +1,9 @@
+package com.planes.android;
+
+/**
+ * Class that manages the control objects: the buttons to position the planes,
+ * the statistics, the buttons to toggle between game board views, start new game
+ * button, button to mark the end of the board editing stage.
+ */
+public class ControlWidgets {
+}
