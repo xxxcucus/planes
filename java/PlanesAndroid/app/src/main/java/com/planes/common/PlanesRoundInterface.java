d79dbe44f10e7559a301ee993eeb7aafd2d66eb3
Cristian Cucu <xxxcucus@yahoo.com>
2020-03-14 11:42:27 +0100

    Added PlanesRoundInterface and empty implementation

---
+++ java/PlanesAndroid/app/src/main/java/com/planes/common/PlanesRoundInterface.java
@@ -0,0 +1,55 @@
+package com.planes.common;
+
+public interface PlanesRoundInterface {
+
+    //creates the PlaneRound object in the game engine
+    //must be called a single time
+    void createPlanesRound();
+
+    //show the planes
+    int getRowNo();
+    int getColNo();
+    int getPlaneNo();
+    int getPlaneSquareType(int i, int j, int isComputer);
+
+    //edit the board
+    int movePlaneLeft(int idx);
+    int movePlaneRight(int idx);
+    int movePlaneUpwards(int idx);
+    int movePlaneDownwards(int idx);
+    int rotatePlane(int idx);
+    void doneClicked();
+
+    //play the game
+    int playerGuessAlreadyMade(int row, int col);
+    void playerGuess(int row, int col);
+    boolean playerGuess_RoundEnds();
+    boolean playerGuess_IsPlayerWinner();
+    boolean playerGuess_ComputerMoveGenerated();
+    int playerGuess_StatNoPlayerMoves();
+    int playerGuess_StatNoPlayerHits();
+    int playerGuess_StatNoPlayerMisses();
+    int playerGuess_StatNoPlayerDead();
+    int playerGuess_StatNoPlayerWins();
+    int playerGuess_StatNoComputerMoves();
+    int playerGuess_StatNoComputerHits();
+    int playerGuess_StatNoComputerMisses();
+    int playerGuess_StatNoComputerDead();
+    int playerGuess_StatNoComputerWins();
+
+    void roundEnds();
+    void initRound();
+
+    //show the guesses
+    int getPlayerGuessesNo();
+    int getPlayerGuessRow(int idx);
+    int getPlayerGuessCol(int idx);
+    int getPlayerGuessType(int idx);
+
+    int getComputerGuessesNo();
+    int getComputerGuessRow(int idx);
+    int getComputerGuessCol(int idx);
+    int getComputerGuessType(int idx);
+
+    int getGameStage();
+}
