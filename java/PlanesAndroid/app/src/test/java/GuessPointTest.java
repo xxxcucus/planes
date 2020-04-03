9e22cdaf1a5725baaad495c5b9a8be45e850eaf6
Cristian Cucu <xxxcucus@yahoo.com>
2020-04-03 20:30:24 +0200

    First unit test common java

---
+++ java/PlanesAndroid/app/src/test/java/GuessPointTest.java
@@ -0,0 +1,11 @@
+import com.google.common.truth.Truth;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class GuessPointTest {
+    @Test
+    public void GuessPoint_SetType() {
+        assertThat(true).isTrue();
+    }
+}
