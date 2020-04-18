c2aa54b7f581104da05e1ca36fc0b24b73df7669
Cristian Cucu <xxxcucus@yahoo.com>
2020-04-18 12:12:31 +0200

    Remove generation of JNI header

--- jni.bat
+++
@@ -1,3 +0,0 @@
-
-echo "Generate jni header"
-javac.exe -h common\ java\PlanesJavaFx\src\com\planes\javafx\PlaneRoundJavaFx.java
--- jni.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-echo "Generate jni header"
-javac -h common java/PlanesJavaFx/src/com/planes/javafx/PlaneRoundJavaFx.java
