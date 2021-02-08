c268ee6b11cfcf6f9b50bac6792191c2f9fc7c87
xxxcucus <xxxcucus@yahoo.com>
2021-02-08 12:41:16 +0100

    Refactored user registration

---
+++ c_plus_plus/multiround/communicationobjects/registercommobj.cpp
@@ -0,0 +1,49 @@
+#include "registercommobj.h"
+#include <QMessageBox>
+#include <QTextCodec>
+#include "viewmodels/loginviewmodel.h"
+
+bool RegisterCommObj::makeRequest(const QString& username, const QString& password)
+{
+    LoginViewModel loginData;
+    loginData.m_Password = password; 
+    loginData.m_UserName = username; 
+
+    m_UserName = username;
+    m_RequestData = loginData.toLoginJson();
+    
+    makeRequestBasis(false);
+    return true;
+}
+
+void RegisterCommObj::finishedRequest()
+{
+    QJsonObject retJson;
+    if (!finishRequestHelper(retJson)) 
+        return;
+
+    std::vector<QString> images;
+    
+    images.push_back(retJson.value("image_id_1").toString());
+    images.push_back(retJson.value("image_id_2").toString());    
+    images.push_back(retJson.value("image_id_3").toString());
+    images.push_back(retJson.value("image_id_4").toString());    
+    images.push_back(retJson.value("image_id_5").toString());
+    images.push_back(retJson.value("image_id_6").toString());    
+    images.push_back(retJson.value("image_id_7").toString());
+    images.push_back(retJson.value("image_id_8").toString());    
+    images.push_back(retJson.value("image_id_9").toString());    
+    long int request_id = retJson.value("id").toString().toLong();
+    
+    qDebug() << "Registration request with id " << request_id << " received ";
+    emit noRobotRegistration(images, retJson);
+
+}
+
+bool RegisterCommObj::validateReply(const QJsonObject& reply) {
+    return (reply.contains("id") && reply.contains("username") && reply.contains("createdAt") && reply.contains("question") && 
+        reply.contains("image_id_1") && reply.contains("image_id_2") && reply.contains("image_id_3") &&
+        reply.contains("image_id_4") && reply.contains("image_id_5") && reply.contains("image_id_6") &&
+        reply.contains("image_id_7") && reply.contains("image_id_8") && reply.contains("image_id_9"));
+
+}
---
+++ c_plus_plus/multiround/communicationobjects/registercommobj.h
@@ -0,0 +1,46 @@
+#ifndef _REGISTER_COMM_OBJ__
+#define _REGISTER_COMM_OBJ__
+
+
+#include "basiscommobj.h"
+
+class RegisterCommObj : public BasisCommObj {
+    Q_OBJECT
+    
+public:
+    RegisterCommObj(const QString& requestPath, const QString& actionName, QNetworkAccessManager* networkManager, QSettings* settings, bool isSinglePlayer, GlobalData* globalData):
+        BasisCommObj(requestPath, actionName, networkManager, settings, isSinglePlayer, globalData) {}
+    
+    bool makeRequest(const QString& username, const QString& password);
+    bool validateReply(const QJsonObject& retJson) override;
+    
+public slots:
+    void finishedRequest() override;       
+    
+signals:
+    void noRobotRegistration(const std::vector<QString>& images, const QJsonObject& request);
+    
+private:
+    QString m_UserName;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
