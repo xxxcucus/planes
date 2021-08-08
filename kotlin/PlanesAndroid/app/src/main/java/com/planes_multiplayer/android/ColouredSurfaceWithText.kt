800f7f0a1066facbf06fa002f0fdf84825cf4aa8
Cristian Cucu <xxxcucus@yahoo.com>
2021-08-08 16:05:51 +0200

    Converted ButtonWithPictogram, ColouredSurfaceWithText, MoveDownButton, MoveLeftButton, MoveRightButton, MoveUpButton, RotateButton to Kotlin

---
+++ kotlin/PlanesAndroid/app/src/main/java/com/planes_multiplayer/android/ColouredSurfaceWithText.kt
@@ -0,0 +1,67 @@
+package com.planes_multiplayer.android
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.planes_multiplayer.android.CanvasPaintUtilities.Functions.computeOptimalTextSizeOneLine
+import com.planes_multiplayer.android.CanvasPaintUtilities.Functions.drawTextFitToSizeOneLine
+import com.planes_multiplayer.android.CanvasPaintUtilities.Functions.measureHeightOneLineText
+import com.planes_multiplayer.android.CanvasPaintUtilities.Functions.measureWidthOneLineText
+import com.planes_multiplayer.android.PlanesVerticalLayout.PlanesVerticalLayoutParams
+
+class ColouredSurfaceWithText : View, ViewWithText {
+    private var m_Paint: Paint? = null
+    private var m_Text: String? = null
+    private var m_TextSize = 10
+    private var m_BackgroundColor = 0
+
+    constructor(context: Context?) : super(context) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
+        init()
+    }
+
+    fun init() {
+        m_Paint = Paint()
+    }
+
+    fun setText(text: String?) {
+        m_Text = text
+        invalidate()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val lp = layoutParams as PlanesVerticalLayoutParams
+        if (m_Text == null) m_Text = lp.text
+        m_BackgroundColor = lp.color
+        val measuredHeight = measureHeightOneLineText(heightMeasureSpec, m_Paint!!, m_Text!!)
+        val measuredWidth = measureWidthOneLineText(widthMeasureSpec, m_Paint!!, m_Text!!)
+        setMeasuredDimension(measuredWidth, measuredHeight)
+    }
+
+    public override fun onDraw(canvas: Canvas) {
+        //TODO: draw the surface in the colour specified
+        m_Paint!!.color = m_BackgroundColor
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), m_Paint!!)
+        m_Paint!!.color = Color.BLUE
+        drawTextFitToSizeOneLine(m_Text!!, m_TextSize, canvas, m_Paint!!, width, height)
+    }
+
+    override fun getOptimalTextSize(maxTextSize: Int, viewWidth: Int, viewHeight: Int): Int {
+        return computeOptimalTextSizeOneLine(m_Text!!, m_Paint!!, viewWidth, viewHeight, maxTextSize)
+    }
+
+    override fun setTextSize(textSize: Int) {
+        m_TextSize = textSize
+        invalidate()
+    }
+}
