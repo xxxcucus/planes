4c19f2033dd8ba8c213715dd59d45bfd2d63bb88
Cristian Cucu <xxxcucus@yahoo.com>
2022-01-12 16:07:43 +0100

    Added multiplayer communication service

--- kotlin/PlanesAndroid/app/src/main/java/com/planes/multiplayer_engine/MultiplayerCommApi.kt
+++ kotlin/PlanesAndroid/app/src/main/java/com/planes/multiplayer_engine/MultiplayerCommApi.kt
@@ -1,5 +1,6 @@
 package com.planes.multiplayer_engine
 
+import com.planes.multiplayer_engine.responses.VersionResponse
 import retrofit2.http.Headers
 import retrofit2.http.POST
 import io.reactivex.Observable
@@ -7,5 +8,5 @@ import io.reactivex.Observable
 interface MultiplayerCommApi {
     @POST("status/getversion")
     @Headers("Content-Type: application/json")
-    fun getVersion(): Observable<retrofit2.Response<VersionResponse>>>
+    fun getVersion(): Observable<retrofit2.Response<VersionResponse>>
 }
