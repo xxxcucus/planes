42cc29d13014eb0d749151ee255687806eb521c9
Cristian <xxxcucus@gmail.com>
2022-11-29 21:35:38 +0100

    Fix compilation under linux

---
+++ c_plus_plus/runTests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+appname=`basename $0 | sed s,\.sh$,,`
+dirname=`dirname $0`
+LD_LIBRARY_PATH=$dirname/lib
+export LD_LIBRARY_PATH
+$dirname/tests/commobjtest "$@"
+$dirname/tests/singleroundtest "$@"
