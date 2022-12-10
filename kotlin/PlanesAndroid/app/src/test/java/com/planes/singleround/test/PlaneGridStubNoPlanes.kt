4c80d7edfef779f46689b50fcc3bbd83fb115164
Cristian Cucu <xxxcucus@yahoo.com>
2022-12-10 06:57:19 +0100

    Refactored tests

--- kotlin/PlanesAndroid/app/src/test/java/com/planes/singleround/test/PlaneGridStubNoPlanes.kt
+++ kotlin/PlanesAndroid/app/src/test/java/com/planes/singleround/test/PlaneGridStubNoPlanes.kt
@@ -1,4 +1,4 @@
-package com.planes.test
+package com.planes.singleround.test.com.planes.singleround.test
 import com.planes.single_player_engine.PlaneGrid
 
 internal class PlaneGridStubNoPlanes(row: Int, col: Int, planesNo: Int, isComputer: Boolean) : PlaneGrid(row, col, planesNo, isComputer) {
