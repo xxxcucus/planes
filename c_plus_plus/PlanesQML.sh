0abfd43b901533445adc06659ff7f6cd059429ca
Cristian <xxxcucus@gmail.com>
2022-12-10 22:30:34 +0100

    Update shell script

--- c_plus_plus/PlanesQML.sh
+++ c_plus_plus/PlanesQML.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 appname=`basename $0 | sed s,\.sh$,,`
 dirname=`dirname $0`
-LD_LIBRARY_PATH=$dirname/lib
+LD_LIBRARY_PATH=$dirname/lib:$dirname/bin:/$dirname/../lib/x86_64-linux-gnu
 export LD_LIBRARY_PATH
 $dirname/bin/$appname "$@"
