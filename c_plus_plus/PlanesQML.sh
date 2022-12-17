2f864fb1d9a00a0e370e0cc7286ba5919be725d7
Cristian <xxxcucus@gmail.com>
2022-12-17 09:24:20 +0100

    Updated shell script

--- c_plus_plus/PlanesQML.sh
+++ c_plus_plus/PlanesQML.sh
@@ -3,6 +3,7 @@ appname=`basename $0 | sed s,\.sh$,,`
 dirname=`dirname $0`
 #export LIBGL_ALWAYS_SOFTWARE=true
 #export QT_XCB_GL_INTEGRATION=none
+export QT_QUICK_BACKEND=software
 LD_LIBRARY_PATH=$dirname/lib
 export LD_LIBRARY_PATH
 $dirname/bin/$appname "$@"
