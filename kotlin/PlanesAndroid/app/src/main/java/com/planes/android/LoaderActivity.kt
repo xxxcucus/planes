3fdada33ce2f9c711fc5c1b725b9a38c641abe30
Cristian Cucu <xxxcucus@yahoo.com>
2023-02-02 21:04:06 +0100

    Created basis loader activity

---
+++ kotlin/PlanesAndroid/app/src/main/java/com/planes/android/LoaderActivity.kt
@@ -0,0 +1,11 @@
+package com.planes.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loader)
+    }
+}
