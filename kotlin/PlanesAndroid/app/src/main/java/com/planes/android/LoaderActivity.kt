6835fe97ddc01eaba321578fa86e3fb31591bd80
Cristian Cucu <xxxcucus@yahoo.com>
2023-02-05 21:11:26 +0100

    Starting single player and multiplayer app

--- kotlin/PlanesAndroid/app/src/main/java/com/planes/android/LoaderActivity.kt
+++ kotlin/PlanesAndroid/app/src/main/java/com/planes/android/LoaderActivity.kt
@@ -1,11 +1,43 @@
 package com.planes.android
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.planes.android.preferences.MainPreferencesServiceGlobal
 
 class LoaderActivity : AppCompatActivity() {
+
+    private var m_MainPreferencesService = MainPreferencesServiceGlobal()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loader)
+
+        m_MainPreferencesService.createPreferencesService(this)
+        m_MainPreferencesService.readPreferences()
+
+        var singlePlayerGameButton = findViewById(R.id.singleplayer) as Button
+        var multiplayerGameButton = findViewById(R.id.multiplayer) as Button
+
+        singlePlayerGameButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                m_MainPreferencesService.multiplayerVersion = false;
+                m_MainPreferencesService.writePreferences()
+                val intent = Intent(this@LoaderActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+        multiplayerGameButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                m_MainPreferencesService.multiplayerVersion = true;
+                m_MainPreferencesService.writePreferences()
+                val intent = Intent(this@LoaderActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
