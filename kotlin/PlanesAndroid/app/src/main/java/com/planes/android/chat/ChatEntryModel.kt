0db79baf7cb88bfe9eef97340987b3eb6095c878
Cristian Cucu <xxxcucus@yahoo.com>
2024-09-05 21:05:43 +0200

    Show user status in the list of players

--- kotlin/PlanesAndroid/app/src/main/java/com/planes/android/chat/ChatEntryModel.kt
+++ kotlin/PlanesAndroid/app/src/main/java/com/planes/android/chat/ChatEntryModel.kt
@@ -1,6 +1,13 @@
 package com.planes.android.chat
 
 import com.planes.multiplayer_engine.responses.UserWithLastLoginResponse
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+import java.util.concurrent.TimeUnit
+
 
 class ChatEntryModel(player: UserWithLastLoginResponse) {
 
@@ -12,5 +19,18 @@ class ChatEntryModel(player: UserWithLastLoginResponse) {
 
     fun getPlayerName(): String {
         return m_Player.m_UserName
+    }
+
+    fun isPlayerOnline(): Boolean {
+        val formatter = SimpleDateFormat("dd MM yyyy HH:mm:ss")
+        formatter.timeZone = TimeZone.getTimeZone("UTC");
+        var formattedDate = m_Player.m_LastLogin
+        val date: Date = formatter.parse(formattedDate)
+
+        return getDateDiff(date, Date.from(Instant.now()), TimeUnit.MINUTES) < 30;
+    }
+    fun getDateDiff(date1: Date, date2: Date, timeUnit: TimeUnit): Long {
+        val diffInMillies: Long = date2.getTime() - date1.getTime()
+        return timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS)
     }
 }
