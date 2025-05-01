2711918671b5d86912a2c0fa3473a0d33d3fe302
Cristian Cucu <xxxcucus@yahoo.com>
2025-05-01 17:32:37 +0200

    Initial commit drawer navigation in PlanesCompose

--- kotlin/PlanesCompose/app/src/main/java/com/planes/android/MainActivity.kt
+++ kotlin/PlanesCompose/app/src/main/java/com/planes/android/MainActivity.kt
@@ -4,14 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
 import com.planes.android.ui.theme.PlanesComposeTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +46,158 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PlanesComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Screen(modifier = Modifier)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Screen(modifier: Modifier) {
+    val drawerState = rememberDrawerState(
+        initialValue = DrawerValue.Closed
+    )
+
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerContent()
+            }
+        },
+        gesturesEnabled = true
+    ) {
+        Scaffold(
+            topBar = {
+                TopBar(
+                    modifier = Modifier.padding(30.dp)
+                        .height(100.dp).fillMaxWidth(),
+                    onOpenDrawer = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed)
+                                    open()
+                                else
+                                    close()
+                            }
+                        }
+                    }
+                )
+            }
+        ) { padding ->
+            ScreenContent(modifier = Modifier.padding(padding))
+        }
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier) {
+
+}
+
+@Composable
+fun DrawerContent(modifier: Modifier = Modifier) {
+    Text(text = "Text App",
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp))
+
+    HorizontalDivider()
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    .size(28.dp))
+        },
+        label = {
+            Text(text = "Item 1",
+                fontSize = 16.sp)
+        },
+        selected = false,
+        onClick = {
+
+        }
+    )
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    .size(28.dp))
+        },
+        label = {
+            Text(text = "Item 2",
+                fontSize = 16.sp)
+        },
+        selected = false,
+        onClick = {
+
+        }
+    )
+
+    NavigationDrawerItem(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    .size(28.dp))
+        },
+        label = {
+            Text(text = "Item 3",
+                fontSize = 16.sp)
+        },
+        selected = false,
+        onClick = {
+
+        }
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    PlanesComposeTheme {
-        Greeting("Android")
-    }
+fun TopBar(modifier: Modifier = Modifier,
+    onOpenDrawer: () -> Unit = {}
+) {
+    TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.clickable {
+                    onOpenDrawer()
+                }. padding(start = 16.dp, end = 8.dp)
+                    .size(28.dp))
+        },
+        title = {
+            Text(text = "Screen Name")
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Default.AccountBox,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.padding(start = 16.dp, end = 8.dp).size(28.dp)
+            )
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = "Navigation Icon",
+                modifier = Modifier.padding(start = 16.dp, end = 8.dp)
+                    .size(28.dp))
+        }
+    )
+}
+
+@Preview
+@Composable
+fun TestTopBar() {
+    TopBar()
 }
