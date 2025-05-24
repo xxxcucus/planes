2f5c9d0159a4af84810674931a01c5d2e7ad2a7d
Cristian Cucu <xxxcucus@yahoo.com>
2025-05-24 15:57:20 +0200

    Preparations tutorials screen

--- kotlin/PlanesCompose/app/build.gradle.kts
+++ kotlin/PlanesCompose/app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
 
+    implementation ("com.google.android.exoplayer:exoplayer:2.19.1")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- kotlin/PlanesCompose/app/src/main/java/com/planes/android/video/VideoScreen.kt
+++ kotlin/PlanesCompose/app/src/main/java/com/planes/android/video/VideoScreen.kt
@@ -1,14 +1,26 @@
 package com.planes.android.video
 
+import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavController
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.planes.android.R
 import com.planes.android.navigation.PlanesScreens
 
 @Composable
@@ -20,6 +32,36 @@ fun VideoScreen(modifier: Modifier, currentScreenState: MutableState<String>,
     Column(modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Video Screen")
+        VideoPlayer()
     }
+}
+
+@Composable
+fun VideoPlayer() {
+    // This is the official way to access current context from Composable functions
+    val context = LocalContext.current
+
+    // Do not recreate the player everytime this Composable commits
+    val exoPlayer = remember(context) {
+        SimpleExoPlayer.Builder(context).build().apply {
+            val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(context,
+                Util.getUserAgent(context, context.packageName))
+
+            val source = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(MediaItem.fromUri( Uri.parse(
+                    "android.resource://"
+                            + context.packageName + "/" + R.raw.guessing)))
+
+            this.prepare(source)
+        }
+    }
+
+    AndroidView(
+        factory = { context ->
+            PlayerView(context).apply {
+                player = exoPlayer
+                //playWhenReady = true
+            }
+        }
+    )
 }
