15444fb120e8d5700c63f7e43580f28de2e86250
Cristian Cucu <xxxcucus@yahoo.com>
2025-05-25 18:10:55 +0200

    Improvements tutorials screen

--- kotlin/PlanesCompose/app/src/main/java/com/planes/android/video/VideoScreen.kt
+++ kotlin/PlanesCompose/app/src/main/java/com/planes/android/video/VideoScreen.kt
@@ -1,26 +1,40 @@
 package com.planes.android.video
 
+import android.content.res.Configuration
 import android.net.Uri
-import android.util.Log
+import android.view.ViewGroup
 import androidx.annotation.OptIn
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat.getString
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DataSource
@@ -38,28 +52,100 @@ fun VideoScreen(modifier: Modifier, currentScreenState: MutableState<String>,
                 navController: NavController, videoModelList: List<VideoModel>) {
 
     currentScreenState.value = PlanesScreens.Tutorials.name
+    val configuration = LocalConfiguration.current
+    val currentVideoState = remember {
+        mutableStateOf(R.raw.guessing)
+    }
 
-    Column(modifier = modifier.fillMaxSize(),
+    Column(
+        modifier = modifier,
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        //TODO: if horizontal place a lazy list near the video
-        Column() {
-            VideoPlayer()
-            LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-                items(items = videoModelList) { entry ->
-                    //TODO: use a card instead of Box
-                    Box() {
-                        Text(text = entry.getVideoName())
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        when (configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+
+                Column() {
+                    VideoPlayer(currentVideoState.value)
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(2),
+                        verticalArrangement = Arrangement.spacedBy(0.dp),
+                        horizontalArrangement = Arrangement.spacedBy(0.dp),
+                        contentPadding = PaddingValues(1.dp)
+                    ) {
+                        items(items = videoModelList) { entry ->
+
+                            Card(
+                                modifier.height(100.dp).width(100.dp).padding(1.dp).
+                                clickable {
+                                    currentVideoState.value = entry.getVideoId()
+                                }.wrapContentHeight(),
+                                shape = RectangleShape,
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                                )
+                            ) {
+                                Column(verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier.fillMaxSize()) {
+                                    Text(
+                                        modifier = Modifier.wrapContentSize()
+                                            .align(Alignment.CenterHorizontally),
+                                        text = entry.getVideoName()
+                                    )
+                                }
+
+                            }
+                        }
                     }
                 }
             }
+
+
+            else -> {
+                Row() {
+                    LazyColumn(
+                        verticalArrangement = Arrangement.spacedBy(0.dp),
+                        contentPadding = PaddingValues(
+                            top = 1.dp,
+                            bottom = 1.dp
+                        )
+                    ) {
+                        items(items = videoModelList) { entry ->
+
+                            Card(
+                                modifier.height(100.dp).width(200.dp).padding(1.dp).
+                                clickable {
+                                    currentVideoState.value = entry.getVideoId()
+                                }.wrapContentHeight(),
+                                shape = RectangleShape,
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                                )
+                            ) {
+                                Column(verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier.fillMaxSize()) {
+                                    Text(
+                                        modifier = Modifier.wrapContentSize()
+                                            .align(Alignment.CenterHorizontally),
+                                        text = entry.getVideoName()
+                                    )
+                                }
+                            }
+                        }
+                    }
+                    VideoPlayer(currentVideoState.value)
+                }
+            }
         }
+
     }
 }
 
 @OptIn(UnstableApi::class)
 @Composable
-fun VideoPlayer() {
+fun VideoPlayer(videoId : Int) {
     // This is the official way to access current context from Composable functions
     val context = LocalContext.current
 
@@ -69,7 +155,7 @@ fun VideoPlayer() {
             val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(context, DefaultHttpDataSource.Factory())
             val uriSource = Uri.parse(
                 "android.resource://"
-                        + context.packageName + "/" + R.raw.guessing)
+                        + context.packageName + "/" + videoId)
             val source = ProgressiveMediaSource.Factory(dataSourceFactory)
                 .createMediaSource(
                     MediaItem.fromUri(uriSource))
@@ -78,11 +164,31 @@ fun VideoPlayer() {
         }
     }
 
+
     AndroidView(
         factory = { ctxt ->
             PlayerView(ctxt).apply {
                 player = exoPlayer
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
             }
+        },
+        update = { pview ->
+            pview.apply {
+                val player = this.player!!
+                val uriSource = Uri.parse(
+                    "android.resource://"
+                            + context.packageName + "/" + videoId)
+                player.setMediaItem(MediaItem.fromUri(uriSource))
+                player.prepare()
+                player.playWhenReady = true
+
+            }
+        },
+        onReset = { pview ->
+            pview.player = null
         }
     )
 }
